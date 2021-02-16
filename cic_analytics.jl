9a49943b8b16a8bca5be690254e09ff68efb7a60
ggsmith842 <ggsmith@ucdavis.edu>
2021-02-16 14:58:56 -0800

    analysis done/put in weave summarise

--- cic_analytics.jl
+++ cic_analytics.jl
@@ -11,12 +11,17 @@ using Statistics
 using TimeSeries
 using Impute: Impute
 using FreqTables
+using HypothesisTests
+using StatsModels
+using Lathe
+using GLM
+
 ##
 
 ##--------
 #load dataset and process
-function process_assets(symbol,period)
-    data = yahoo(symbol, YahooOpt(period1 =(now() - period),period2 = now()- Day(1)))
+function process_assets(symbol,period) #gotta figure out how to fix date
+    data = yahoo(symbol, YahooOpt(period1 =(now() - period),period2 =now()-Day(4)))
     returns = percentchange(data.Close)
     merged_data = merge(data,returns.*100)
     merged_data= TimeSeries.rename(merged_data::TimeArray, :Close_1 => :Return)
@@ -25,9 +30,9 @@ function process_assets(symbol,period)
     return merged_data, merged_df
 end
 
-eth, eth_df = process_assets(string("ETH-USD"),Month(1))
-lnk, lnk_df = process_assets(string("LINK-USD"),Month(1))
-snp, snp_df = process_assets(string("^GSPC"),Month(1))
+eth, eth_df = process_assets(string("ETH-USD"),Month(2))
+lnk, lnk_df = process_assets(string("LINK-USD"),Month(2))
+snp, snp_df = process_assets(string("^GSPC"),Month(2))
 #eth = yahoo(:"ETH-USD", YahooOpt(period1 = now() - Month(1)))
 
 ##---
@@ -35,23 +40,22 @@ snp, snp_df = process_assets(string("^GSPC"),Month(1))
 
 ##-- candlestick and MAs function
 function crypto_MAs(ts,df,n1=7,n2=2)
-    plot(ts, seriestype = :candlestick)
+    plot(ts, seriestype = :candlestick,title="Candlestick")
     movingaverage=sma(sort(df, :timestamp).Close, n=n1)
     short_MA = sma(sort(df, :timestamp).Close, n=n2)
     plot!(movingaverage, linewidth=2, color=:black)
-    plot!(short_MA, linewidth=2, color=:blue)
+    plot!(short_MA, linewidth=2, color=:blue,label=n2)
 
     end
 ##--
 # Work on eth EDA
-print(describe(eth_df))
-sigma_sq = var(eth_df.Close)
+print(describe(eth_df),"\n")
 sigma = std(eth_df.Close)
-print("std dev: ",sigma)
+print("sigma: ",sigma)
 #candlestick with MA
 crypto_MAs(eth,eth_df)
 #Plot density
-histogram(eth_df[:,"Returns"],bins=15,label="Returns")
+histogram(eth_df[:,"Return"],bins=15,label="Returns")
 #its interesteing that there's a gap between 1400-1500 price
 ##----
 
@@ -60,66 +64,125 @@ histogram(eth_df[:,"Returns"],bins=15,label="Returns")
 crypto_MAs(lnk,lnk_df)
 histogram(lnk.Close,bins=25,label="Close")
 
-print(describe(lnk_df))
-
-print(var(lnk_df.Close))
-print(std(lnk_df.Close))
-
-
-
+print(describe(lnk_df),"\n","sigma ",std(lnk_df.Close))
 ##--
 
 ##--
 crypto_MAs(snp,snp_df)
 histogram(snp.Close,bins=30,label="Close")
 
-print(describe(snp_df))
+print(describe(snp_df),"\n","sigma ",std(snp_df.Close))
 
-print(var(snp_df.Close))  #3582.6
-print(std(snp_df.Close)) #59.85
 ##--
 
 ##--
-#Combine close from eth and snp to test for correlation
-#Crypto trades on the weekends resulting in more prices
-#Will result in a loss of data;use last value immupation for snp
-
 eth_snp_df= leftjoin(eth_df,snp_df, on = :timestamp,makeunique=true)
 closing_df = eth_snp_df[:,["Close","Close_1"]]
 
-eth_snp_df = Impute.locf(eth_snp_df)
-closing_df = Impute.locf(closing_df)
-#have to replace missing values next
-#then normalize
+eth_snp_df = Impute.nocb(eth_snp_df)
+closing_df = Impute.nocb(closing_df)
+
+eth_lnk_df = leftjoin(eth_df,lnk_df, on = :timestamp,makeunique=true)
+eth_lnk_df_close = eth_lnk_df[:,["Close","Close_1"]]
 
 ##--
 
 ##--
+# ETH vs SNP
 gr()
 plot(closing_df.Close,closing_df.Close_1, seriestype = :scatter, title = "S&P Close and ETH Close")
 #no correlation between returns for snp and eth
 @df eth_snp_df cor(:Return,:Return_1)
 #moderate correlation in closing prices
 @df eth_snp_df cor(:Close,:Close_1)
+
+#ETH vs LNK
+plot(eth_lnk_df_close.Close,eth_lnk_df_close.Close_1,
+    seriestype = :scatter, title = "ETH Close and LNK Close",label="Close")
+#no correlation between returns for snp and eth
+@df eth_lnk_df cor(:Return,:Return_1)
+#good correlation in closing prices
+@df eth_lnk_df cor(:Close,:Close_1)
+
+##--
+#### Next step is to normalize returns and check scatter plot/corr
+#function to normalize a pair of columns ie eth and snp returns
+function normalize_column(arr)
+    dt=StatsBase.fit(UnitRangeTransform, arr; dims=1, unit=true)
+    dt_norm = StatsBase.transform(dt,arr)
+    return dt_norm
+end
+
+eth_norm_close = normalize_column(eth_df.Close)
+lnk_norm_close = normalize_column(lnk_df.Close)
+
+eth_lnk_norm_close = DataFrame(eth=eth_norm_close,lnk=lnk_norm_close)
+#plot(eth_norm_returns,snp_norm_returns, seriestype = :scatter, title = "S&P Return and ETH Return")
+#no correlation at all with returns
+
+##--
+#Run linear regression on lnk and eth
+train, test = Lathe.preprocess.TrainTestSplit(eth_lnk_norm_close,.70)
+
+fm = @formula(eth ~ lnk)
+linearRegressor = lm(fm, train)
+print(linearRegressor)
+
+gr()
+plot(eth_lnk_norm_close.lnk,eth_lnk_norm_close.eth,
+    seriestype = :scatter, title = "ETH Close and LNK Close",label="Close",legend=:bottomright)
+plot!((x) -> -0.06898 + 1.1825 * x, 0, 1, label="fit_exact")
 ##--
 
-plot(boxplot(closing_df.Close_1),boxplot(closing_df.Close))
-
-#### NExt step is to normalize returns and check scatter plot/corr
-#### After that check relationship between link and eth
+ypredicted_test = predict(linearRegressor, test)
 
 
-
+#predicted vs actual
+DataFrame(eth_pred=ypredicted_test,eth_actual=test.eth)
 
 ##--
 #Bayesian Inference section
-eth_pos_returns = ((eth_df.Return) .> 0)
-snp_pos_returns = ((snp_df.Return) .> 0)
+#Build function to calculate BI probability
 
-DataFrame(Decline=[length(eth_pos_returns)-sum(eth_pos_returns),
-    length(snp_pos_returns)-sum(snp_pos_returns),12+8],
-    Increase=[sum(eth_pos_returns),sum(snp_pos_returns),18+13],
-    Totals = [12+18,8+13,20+31])
-nrow(eth_df)+nrow(snp_df)
+#function-----------------------
+function bayes_prob(df1,df2,increase=true,complement=false)
+
+    df = leftjoin(df1,df2, on = :timestamp,makeunique=true)
+    if sum(describe(df).nmissing) > 0
+        df = Impute.nocb(df) #in case missing values exist
+
+    end
+
+    return_df = DataFrame(A=df.Return.>0,B=df.Return_1.>0)
+    my_table = freqtable(return_df.B,return_df.A)
+
+    if increase == true
+        prob_a = sum(my_table[:,Name(true)])/sum(my_table)
+        prob_b = sum(my_table[Name(true),:])/sum(my_table)
+        prob_ba = sum(my_table[Name(true),Name(true)])/sum(my_table[:,Name(true)])
+
+        prob_ab = prob_ba*prob_a/prob_b
+    else
+        prob_a = sum(my_table[:,Name(true)])/sum(my_table)
+        prob_b = sum(my_table[Name(false),:])/sum(my_table)
+        prob_ba = sum(my_table[Name(false),Name(true)])/sum(my_table[:,Name(true)])
+
+        prob_ab = prob_ba*prob_a/prob_b
+    end
+
+    if complement==false
+        return prob_ab
+    else
+        return 1-prob_ab
+    end
+end
+
+bayes_prob(eth_df,lnk_df)
+
+#end function----------------------------
+
+##--
+#put in weave
+#summarize findings and contents
 
 ##--
